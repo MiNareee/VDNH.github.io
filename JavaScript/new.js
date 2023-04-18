18ac2abc6d9e50c758e2f7187319da44a096a5d9
MiNareee <109744081+MiNareee@users.noreply.github.com>
2023-04-18 08:51:29 +0300

    Update new.js

--- JavaScript/new.js
+++ JavaScript/new.js
@@ -189,7 +189,7 @@ function alerted(){
 	var TmeNov=[]
 	let Time_Right=0
 	for (let i=0;i<fs.length;i++){
-  let Time_Now=['Сб','06:20']
+  let Time_Now=['Сб','10:20']
   let Timers=fs[i]
   Time_Right=TimeOclock(Time_Now,Timers)
   if (Time_Right<0){
